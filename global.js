c7ec632b2e120e70c39714c6f916d1e407059534
Ndivhuwo <138808337+XBCIS-Group@users.noreply.github.com>
2023-08-18 23:44:26 +0200

    Updated Code

--- global.js
+++ global.js
@@ -35,3 +35,29 @@ document.addEventListener("DOMContentLoaded", function() {
       contentSection.style.transform = "translateY(0)";
     }, 500);
 });
+
+new Vue({
+  el: '#app',
+  data: {
+    images: [
+      { src: 'image1.jpg', title: 'Image 1 Title' },
+      { src: 'image2.jpg', title: 'Image 2 Title' },
+      { src: 'image3.jpg', title: 'Image 3 Title' },
+      { src: 'image4.jpg', title: 'Image 4 Title' },
+      { src: 'image5.jpg', title: 'Image 5 Title' },
+      { src: 'image6.jpg', title: 'Image 6 Title' },
+      { src: 'image7.jpg', title: 'Image 7 Title' }
+    ],
+    translateValue: 0
+  },
+  methods: {
+    scrollCarousel(direction) {
+      const step = 300; // Adjust this value for smoother/faster scrolling
+      if (direction === 'left') {
+        this.translateValue += step;
+      } else if (direction === 'right') {
+        this.translateValue -= step;
+      }
+    }
+  }
+});
