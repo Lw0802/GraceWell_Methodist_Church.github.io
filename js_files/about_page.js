d8197d39df0aa6daa08bb7ae25f53bcb6693d3a5
Ndivhuwo <138808337+XBCIS-Group@users.noreply.github.com>
2023-09-07 23:35:10 +0200

    Code Update

--- js_files/about_page.js
+++ js_files/about_page.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () { 
-    
-});
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
