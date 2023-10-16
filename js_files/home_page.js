cd0add0600fc4359a8f34bfd70795b3389018285
Ndivhuwo <138808337+XBCIS-Group@users.noreply.github.com>
2023-10-16 13:19:53 +0200

    Video functionality

--- js_files/home_page.js
+++ js_files/home_page.js
@@ -6,37 +6,37 @@ document.addEventListener("DOMContentLoaded", function () {
         sermons: [
           {
             id: 1,
-            imageSrc: "././website_images/pexels-pixabay-the-cross.jpg",
+            videoSrc: "https://www.facebook.com/GraceWellMC/videos/1126375311659767/?mibextid=zDhOQc/",
             title: "Sermon Title 1"
           },
           {
             id: 2,
-            imageSrc: "././website_images/pexels-pixabay-the-cross.jpg",
+            videoSrc: "https://fb.watch/nIR4RzHB0l/",
             title: "Sermon Title 2"
           },
           {
             id: 3,
-            imageSrc: "././website_images/pexels-pixabay-the-cross.jpg",
+            videoSrc: "https://fb.watch/nIR80-qTM9/",
             title: "Sermon Title 3"
           },
           {
             id: 4,
-            imageSrc: "././website_images/pexels-pixabay-the-cross.jpg",
+            videoSrc: "https://fb.watch/nIRbL8QjiJ/",
             title: "Sermon Title 4"
           },
           {
             id: 5,
-            imageSrc: "././website_images/pexels-pixabay-the-cross.jpg",
+            videoSrc: "././website_images/pexels-pixabay-the-cross.jpg",
             title: "Sermon Title 5"
           },
           {
             id: 6,
-            imageSrc: "././website_images/pexels-pixabay-the-cross.jpg",
+            videoSrc: "././website_images/pexels-pixabay-the-cross.jpg",
             title: "Sermon Title 6"
           },
           {
             id: 7,
-            imageSrc: "././website_images/pexels-pixabay-the-cross.jpg",
+            videoSrc: "././website_images/pexels-pixabay-the-cross.jpg",
             title: "Sermon Title 7"
           }
         ]
